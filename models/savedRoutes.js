bde9cf9adf556d7dc566375b811580357a5a54e8
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-25 19:37:28 -0500

    create schema for routes DB

---
+++ models/savedRoutes.js
@@ -0,0 +1,14 @@
+// const mongoose = require('mongoose');
+//
+// const savedRoutesSchema = new mongoose.Schema({
+//   origin: {type: String, required: true},
+//   destination: {type: String, required: true},
+//   waypoints: Array,
+//   optimize: {type: Boolean, required: true},
+//   mode: {type: String, required: true}
+//
+// })
+//
+// const savedRoutes = mongoose.model('savedRoutes', savedRoutesSchema);
+//
+// module.exports = savedRoutes;
