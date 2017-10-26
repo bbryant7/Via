ebd43906264f8636989b0d44fb11173c1c480408
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-26 18:33:47 -0500

    add suggestion page

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ const session = require('express-session');
 const userDataSchema = require('./models/userData')
 const app = express();
 
+let data = [{
+  "id": 1,
+  "title":"North Austin Breweries",
+  "details":"lkajhsfklahjsdlfkjahsdlkfhaslkfjhaslkfjhaslkjfhaslkjhfalskjhfaskjhfaklsjhflaskhfalkjhsflasjkhfdlkasjhflkasjhfljhsalkjhflakshfdlaksjhdf",
+  "img":"https://images.unsplash.com/photo-1438557068880-c5f474830377?w=1353&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
+
+},
+{
+  "id": 2,
+  "title":"Explore Soco District",
+  "details":"lkajhsfklahjsdlfkjahsdlkfhaslkfjhaslkfjhaslkjfhaslkjhfalskjhfaskjhfaklsjhflaskhfalkjhsflasjkhfdlkasjhflkasjhfljhsalkjhflakshfdlaksjhdf",
+  "img":"http://www.videocityguide.com/austin/PCWUploads/South%20Congress%20Avenue%20Shopping/g3.jpg",
+
+},
+{
+  "id": 3,
+  "title":"South-side Spots",
+  "details":"lkajhsfklahjsdlfkjahsdlkfhaslkfjhaslkfjhaslkjfhaslkjhfalskjhfaskjhfaklsjhflaskhfalkjhsflasjkhfdlkasjhflkasjhfljhsalkjhflakshfdlaksjhdf",
+  "img":"http://www.videocityguide.com/austin/PCWUploads/South%20Congress%20Avenue%20Shopping/thumbnails/g14.jpg",
+
+}]
+// /---------------------------/
+
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
@@ -92,13 +115,13 @@ app.get('/', function(req, res) {
   })
 
 // SUGGESTIONPAGE
-  app.get('/routesuggestions', function(req, res) {
-    // data.findOne().where({
-    //   _id: (req.params.id)
-
-      res.render('routesuggestions')
+app.get('/routesuggestions', function(req, res) {
+  // data.findOne().where({
+  //_id: (req.params.id)
+    console.log("data",data);
+    res.render('routesuggestions', {route: data})
     })
-    // console.log(req.params.id);
+// console.log(req.params.id);
   // });
 
 app.listen(3000, function() {
--- models/savedRoutes.js
+++ models/savedRoutes.js
@@ -30,3 +30,20 @@ directionsService.route({
   }
 });
 }
+
+id: 1,
+"title":"North Austin Breweries",
+// "stops": [
+//   "Origin": "2314 Rutland Dr Austin, Tx",
+//   "Destination": "3001 Industrial Terrace Austin, TX",
+//   "wapoints": [
+//   {
+//     "location": "10615 Metric Blvd Austin, TX",
+//     "stopover": true
+//   },{
+//     "location": "10420 Metric Blvd Austin, TX",
+//     "stopover": true
+//   }
+//   ],
+  "optimize": true,
+  "travelMode":'DRIVING',
